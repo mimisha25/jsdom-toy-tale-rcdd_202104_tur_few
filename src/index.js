87efe218381ca318c5af6ff0c06a365581e6a780
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 22:45:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,51 +1,90 @@
-document.addEventListener("DOMContentLoaded", () => {
+let addToy = false;
+
+ document.addEventListener("DOMContentLoaded", () => {
    const addBtn = document.querySelector("#new-toy-btn");
    const toyFormContainer = document.querySelector(".container");
-   const toy-collection = document.getElementById('toy-collection');
-   const form = document.querySelector(".add-toy-form");
-   const option ={
-     method :"POST",
-     headers:
-     {
-     "Content-Type": "application/json",
-     Accept: "application/json"
-     }
-
-     body: JSON.stringify({
-     "name": "Jessie",
-     "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-     "likes": 0
-     })
-   };
- const op = {
-   method:"PATCH",
-   headers:
- {
-   "Content-Type": "application/json",
-   Accept: "application/json"
- }
-
- body: JSON.stringify({
-   "likes": ++(json.likes)
- })
- }
-
- form.addEventListener("submit" , ()=>{
-   fetch("http://localhost:3000/toys" , option)
-   .then(response => response.json())
-   .then(json =>{
-     toy-collection.insertAdjacentHTML("beforeend",`<div class='card'><h2>${json.name}</h2><img src=${json.image} class='toy-avatar' /><p>${json.likes} Likes </p><button class='like-btn'>Like <3</button> </div>`)
-   });
- })
-
-   fetch("http://localhost:3000/toys")
-   .then(response => response.json())
-   .then(json =>{
-     toy-collection.insertAdjacentHTML("beforeend",`<div class='card'><h2>${json.name}</h2><img src=${json.image} class='toy-avatar' /><p>${json.likes} Likes </p><button class='like-btn'>Like <3</button> </div>`)
-     fetch("http://localhost:3000/toys" , op)
-   });
-
-
    addBtn.addEventListener("click", () => {
      // hide & seek with the form
      addToy = !addToy;
+     if (addToy) {
+       toyFormContainer.style.display = "block";
+     } else {
+       toyFormContainer.style.display = "none";
+     }
+   });
+
+   const likes = (e) => {
+     e.preventDefault();
+     const add = parseInt(e.target.previousElementSibling.innerText) + 1;
+
+     fetch(`http://localhost:3000/toys/${e.target.id}`, {
+       method: "PATCH",
+       headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+       },
+       body: JSON.stringify({
+         likes: add,
+       }),
+     })
+       .then((r) => r.json())
+       .then((d) => {
+         e.target.previousElementSibling.innerText = `${add} likes`;
+       })
+       .catch((e) => console.log(e));
+   };
+
+   const renderToys = (toy) => {
+     const toyCollection = document.getElementById("toy-collection");
+     const h2 = document.createElement("h2");
+     h2.innerText = toy.name;
+
+     const img = document.createElement("img");
+     img.setAttribute("src", toy.image);
+     img.classList.add("toy-avatar");
+
+     const p = document.createElement("p");
+     p.innerText = `${toy.likes} likes`;
+
+     const btn = document.createElement("button");
+     btn.classList.add("like-btn");
+     btn.setAttribute("id", toy.id);
+     btn.innerText = "like";
+
+     btn.onclick = (e) => likes(e);
+
+     const card = document.createElement("div");
+     card.classList.add("card");
+     card.append(h2, img, p, btn);
+     toyCollection.append(card);
+   };
+
+   const getToys = () =>
+     fetch("http://localhost:3000/toys")
+       .then((r) => r.json())
+       .then((d) => d.forEach((toy) => renderToys(toy)));
+
+   getToys();
+
+   const postToy = (name, image) =>
+     fetch("http://localhost:3000/toys", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+       },
+       body: JSON.stringify({
+         name,
+         image,
+         likes: 0,
+       }),
+     })
+       .then((r) => r.json())
+       .then((d) => renderToys(d));
+
+   toyFormContainer.onsubmit = (e) => {
+     e.preventDefault();
+     const inputs = document.getElementsByClassName("input-text");
+     postToy(inputs[0].value, inputs[1].value);
+   };
+ });
